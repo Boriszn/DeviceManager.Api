f37ead4b0af478408b5966d6e4d6a624bc3668cb
boriszn <boris.zaikin@re-flekt.com>
2018-04-22 21:17:54 +0200

    Add test context factory; Create API integration tests

---
+++ test/DeviceManager.Api.UnitTests/Api/DevicesApiTests.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using DeviceManager.Api.UnitTests.Api.Server;
+using DeviceManager.Api.UnitTests.Builders;
+using FluentAssertions;
+using Xunit;
+
+namespace DeviceManager.Api.UnitTests.Api
+{
+    [Category("Integration")]
+    public class DevicesApiTests
+    {
+        [Fact]
+        public async Task GetDevices_WithValidParameters_ReturnsOkResult()
+        {
+            // Arrange and Act
+            var devicesApiBuilder = await new DevicesApiBuilder()
+                .QueryWith(page: 1, pageCount: 5, version:"1.0")
+                .WithTenantId("b0ed668d-7ef2-4a23-a333-94ad278f45d7")
+                .Get();
+
+            // Assert
+            devicesApiBuilder.HttpResponseMessage.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+
+        [Fact]
+        public async Task GetDevices_WithoutTenantId_ReturnsInternalServerErrorResult()
+        {
+            // Arrange and Act
+            var devicesApiBuilder = await new DevicesApiBuilder()
+                .QueryWith(page: 1, pageCount: 5, version: "1.0")
+                .Get();
+
+            // Assert
+            devicesApiBuilder.HttpResponseMessage.StatusCode
+                .Should().Be(HttpStatusCode.InternalServerError);
+        }
+    }
+}
---
+++ test/DeviceManager.Api.UnitTests/Builders/DevicesApiBuilder.cs
@@ -0,0 +1,61 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using DeviceManager.Api.UnitTests.Api.Server;
+
+namespace DeviceManager.Api.UnitTests.Builders
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class DevicesApiBuilder
+    {
+        private readonly TestContext testContext;
+
+        private string query;
+        public HttpResponseMessage HttpResponseMessage;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DevicesApiBuilder"/> class.
+        /// </summary>
+        public DevicesApiBuilder()
+        {
+            testContext = new TestContext();
+        }
+
+        /// <summary>
+        /// Queries with parameters.
+        /// </summary>
+        /// <param name="page">The page.</param>
+        /// <param name="pageCount">The page count.</param>
+        /// <param name="version">The version.</param>
+        /// <returns></returns>
+        public DevicesApiBuilder QueryWith(int page, int pageCount, string version)
+        {
+            query = $"api/v{version}/devices?page={page}&pageSize={pageCount}";
+
+            return this;
+        }
+
+        /// <summary>
+        /// Withs the tenant identifier.
+        /// </summary>
+        /// <param name="tenantId">The tenant identifier.</param>
+        /// <returns></returns>
+        public DevicesApiBuilder WithTenantId(string tenantId)
+        {
+            testContext.Client.DefaultRequestHeaders.Add("tenantid", tenantId );
+
+            return this;
+        }
+
+        /// <summary>
+        /// Http Get devices
+        /// </summary>
+        /// <returns></returns>
+        public async Task<DevicesApiBuilder> Get()
+        {
+            HttpResponseMessage = await testContext.Client.GetAsync(query);
+            return this;
+        }
+    }
+}
